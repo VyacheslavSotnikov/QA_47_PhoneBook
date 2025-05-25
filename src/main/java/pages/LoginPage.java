8835ab797fb9b71f1e1480a324ff336251bc4d19
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-05-25 21:55:12 +0300

    added pages + @test

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends  BasePage{
+
+    public LoginPage(WebDriver driver){
+        setDriver(driver);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
+    }
+
+    @FindBy(name = "email")
+    WebElement inputEmail;
+
+    @FindBy(name = "password")
+    WebElement inputPassword;
+
+    @FindBy(xpath = "//button[text()='Login']")
+    WebElement btnLoginBody;
+
+    public void typeLoginForm(String email, String password){
+        inputEmail.sendKeys(email);
+        inputPassword.sendKeys(password);
+        btnLoginBody.click();
+    }
+}
---
+++ src/test/java/ui_tests/HomeTests.java
@@ -0,0 +1,13 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class HomeTests extends ApplicationManager {
+
+    @Test
+    public void FirstTest(){
+        HomePage homePage = new HomePage(getDriver())
+;    }
+}
