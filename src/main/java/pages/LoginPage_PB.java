3cc8f6898c207a208dbbb426826ecf62a2469483
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-06-04 21:17:53 +0300

    added pages + @test

--- src/main/java/pages/LoginPage_PB.java
+++ src/main/java/pages/LoginPage_PB.java
@@ -1,11 +1,16 @@
 package pages;
 
 import dto.User;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage_PB extends BasePage_PB {
 
@@ -23,6 +28,9 @@ public class LoginPage_PB extends BasePage_PB {
     @FindBy(xpath = "//button[text()='Login']")
     WebElement btnLoginBody;
 
+    @FindBy(xpath = "//div[@class='login_login__3EHKB']/div")
+    WebElement errorMassageLogin;
+
 
     public void typeLoginForm (User user){
         inputEmail.sendKeys(user.getUsername());
@@ -33,4 +41,16 @@ public class LoginPage_PB extends BasePage_PB {
 //        inputPassword.sendKeys(password);
 //        btnLoginBody.click();
     }
+
+    public void closeAlert(){
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5)).
+                until(ExpectedConditions.alertIsPresent());
+        System.out.println(alert.getText());
+        alert.accept();
+    }
+
+    public boolean isErrorMassagePresent(String message){
+        return isTextInElementPresent(errorMassageLogin, message);
+    }
+
 }
--- src/test/java/ui_tests/LoginTests_PB.java
+++ src/test/java/ui_tests/LoginTests_PB.java
@@ -2,11 +2,42 @@ package ui_tests;
 
 import dto.User;
 import manager.ApplicationManager_PB;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.ContactsPage;
 import pages.HomePage_PB;
 import pages.LoginPage_PB;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class LoginTests_PB extends ApplicationManager_PB {
+
+    @Test
+    public void loginNegativeTest_wrongPassword() {
+        User user = new User ("sotiga2015@gmail.com", "Sh12345!2");
+        HomePage_PB homePagePB = new HomePage_PB((getDriver()));
+        homePagePB.clickBtnLoginHeader();
+        LoginPage_PB loginPagePB = new LoginPage_PB(getDriver());
+        loginPagePB.typeLoginForm(user);
+        loginPagePB.closeAlert();
+        Assert.assertTrue(loginPagePB.isErrorMassagePresent("Login Failed with code 401"));
+
+    }
+
+    @Test
+    public void loginNegativeTest_wrongEmail() {
+        User user = new User ("sotiga2015gmail.com", "Sh12345!2");
+        HomePage_PB homePagePB = new HomePage_PB((getDriver()));
+        homePagePB.clickBtnLoginHeader();
+        LoginPage_PB loginPagePB = new LoginPage_PB(getDriver());
+        loginPagePB.typeLoginForm(user);
+        loginPagePB.closeAlert();
+        Assert.assertTrue(loginPagePB.isErrorMassagePresent("Login Failed with code 401"));
+    }
 
     @Test
     public void loginPositiveTest() {
