3141607569ed0bb3591443a7bfe64ca5c25e1634
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-07-09 19:35:53 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/dto/Contact.java
@@ -0,0 +1,34 @@
+package dto;
+
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Random;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    private String id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(lastName, contact.lastName) && Objects.equals(email, contact.email) && Objects.equals(phone, contact.phone) && Objects.equals(address, contact.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName, email, phone, address);
+    }
+}
